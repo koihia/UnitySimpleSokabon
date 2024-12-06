aafc318a0d0ae63b26e4c1b9fec0b4ac9d6fd1f2
Brian Gao <30551185+koihia@users.noreply.github.com>
2024-12-06 13:43:43 +0800

    Add two-level level list with scroll view

--- Assets/Scripts/UI/UILevelList.cs
+++ Assets/Scripts/UI/UILevelList.cs
@@ -1,11 +1,13 @@
 ﻿using UnityEngine;
+using Debug = System.Diagnostics.Debug;
 
 namespace Sokabon.UI
 {
     public class UILevelList : MonoBehaviour
     {
         public LevelManager levelManager;
-        public GameObject levelButtonPrefab;
+        public GameObject levelListPrefab;
+        public UILevelButton levelButtonPrefab;
 
         private void Awake()
         {
@@ -17,10 +19,20 @@ namespace Sokabon.UI
 
         private void Start()
         {
+            GameObject levelList = null;
+            var prevSection = "-1";
             foreach (var level in levelManager.Levels)
             {
-                var levelButton = Instantiate(levelButtonPrefab, transform);
-                levelButton.GetComponent<UILevelButton>().SetLevel(levelManager, level);
+                var section = level.LevelNumber.Split('-')[0];
+                if (section != prevSection)
+                {
+                    prevSection = section;
+                    levelList = Instantiate(levelListPrefab, transform);
+                }
+
+                Debug.Assert(levelList != null, nameof(levelList) + " != null");
+                var levelButton = Instantiate(levelButtonPrefab, levelList.transform);
+                levelButton.SetLevel(levelManager, level);
             }
         }
     }
