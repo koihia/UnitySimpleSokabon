12c82e0befa6579209df977924db35ed2aa907e7
Brian Gao <30551185+koihia@users.noreply.github.com>
2024-12-07 18:13:45 +0800

    Add stars indicator in level selection button

--- Assets/Scripts/UI/UILevelButton.cs
+++ Assets/Scripts/UI/UILevelButton.cs
@@ -1,5 +1,6 @@
 ﻿using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Sokabon.UI
 {
@@ -8,12 +9,24 @@ namespace Sokabon.UI
         public LevelManager levelManager;
         public TextMeshProUGUI levelNumberText;
         private Level _level;
+
+        [SerializeField] private Image starsIndicator1;
+        [SerializeField] private Image starsIndicator2;
+        [SerializeField] private Image starsIndicator3;
+        
+        [SerializeField] private Color starColor;
+        [SerializeField] private Color emptyStarColor;
         
         public void SetLevel(LevelManager levelMgr, Level level)
         {
             levelManager = levelMgr;
             levelNumberText.text = level.levelNumber;
             _level = level;
+            
+            var starsCount = GameDataManager.GetStarsCount(level, GameDataManager.GetBestMoves(level));
+            starsIndicator1.color = starsCount >= 1 ? starColor : emptyStarColor;
+            starsIndicator2.color = starsCount >= 2 ? starColor : emptyStarColor;
+            starsIndicator3.color = starsCount >= 3 ? starColor : emptyStarColor;
         }
         
         public void OnClick()
