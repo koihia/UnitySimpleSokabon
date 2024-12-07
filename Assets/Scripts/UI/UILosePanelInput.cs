a8027e325386552633f33f39b2c8a42d3ad45b04
Brian Gao <30551185+koihia@users.noreply.github.com>
2024-12-07 17:58:35 +0800

    Undo with Z in lose panel

---
+++ Assets/Scripts/UI/UILosePanelInput.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+
+namespace Sokabon.UI
+{
+    public class UILosePanelInput : MonoBehaviour
+    {
+        [SerializeField] private TurnManager turnManager;
+        
+        private void Awake()
+        {
+            turnManager ??= FindObjectOfType<TurnManager>();
+        }
+        
+        public void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Z))
+            {
+                turnManager.Undo();
+            }
+        }
+    }
+}
